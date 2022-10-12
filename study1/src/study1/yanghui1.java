186d6ca12ac9b3b510ebf22b85a3f293ead5659f
Jiawei Zhou <zhou.jiaw@northeastern.edu>
2022-10-12 11:36:32 -0600

    yanghui

---
+++ study1/src/study1/yanghui1.java
@@ -0,0 +1,24 @@
+package study1;
+
+public class yanghui1 {
+
+	public static void main(String[] args) {
+		int[][] yanghui = new int[10][];
+		for (int i = 0; i <yanghui.length; i++) {
+			yanghui[i] = new int[i+1];
+			for (int j = 0; j<yanghui[i].length;j++) {
+				if (j ==0 || j == yanghui[i].length - 1) {
+					yanghui[i][j] = 1;
+				} else {
+					yanghui[i][j] = yanghui[i-1][j] + yanghui[i-1][j-1];
+				}
+			}
+		}
+		for (int i = 0; i <yanghui.length; i++) {
+			for (int j = 0; j <yanghui[i].length; j++) {
+		System.out.print(yanghui[i][j] + "\t" );
+			}
+			System.out.println();
+	}
+}
+}
